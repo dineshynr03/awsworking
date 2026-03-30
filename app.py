e404fcc8319beecfb3dd9795811f9a519becebae
EC2 Default User <ec2-user@ip-172-30-0-122.ap-south-1.compute.internal>
2026-03-30 14:54:25 +0000

    v2: updated

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Docker + GitHub Actions!--dinesh 1.0 dev"
+    return "version 2: auto deployed -->
+Hello from Docker + GitHub Actions!--dinesh 1.0 dev"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
